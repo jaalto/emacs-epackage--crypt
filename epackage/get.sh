4e6032070b016e0b1aa5abb5e47feeed97d6dc13
Jari Aalto <jari.aalto@cante.net>
2010-11-27 03:03:52 +0200

    epackage/get.sh: update

--- epackage/get.sh
+++ epackage/get.sh
@@ -1,1 +1,9 @@
-wget 'http://cvs.xemacs.org/viewcvs.cgi/*checkout*/XEmacs/packages/xemacs-packages/os-utils/crypt.el?rev=HEAD&content-type=text/plain' -O crypt.el
+#!/bin/sh
+
+set -e
+
+pwd=$(cd $(dirname $0); pwd)
+url=$( awk '/^Vcs-Url:/  {print $2}' "$pwd/info" )
+
+wget --no-check-certificate --timestamping \
+     -O crypt.el "$url"
